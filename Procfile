ebda42254c8710fbb8a0364c3087b92d23a95b05
Apoorv Pandey <apoorvpandey0@gmail.com>
2020-06-20 13:36:05 +0530

    some changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn support.wsgi
+web: gunicorn cstsupport.wsgi
--- support/settings.py
+++ support/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*2v812fbtw@q0%#aw_87zof65)!t0uy!=1gq64p^ji370*r!-l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cstsupport.herokuapp.com','localhost']
 
 
 # Application definition
